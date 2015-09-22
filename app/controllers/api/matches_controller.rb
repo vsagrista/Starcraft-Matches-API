2978ee6e6135523e66c1547b0b375bfa57ad8d83
Victor Sagrista Lopez <victorsagristalopez@gmail.com>
2015-09-22 16:15:36 +0200

    First part of second iteration, created index page that displays matches of selected player

--- app/controllers/api/matches_controller.rb
+++ app/controllers/api/matches_controller.rb
@@ -1,11 +1,25 @@
 class Api::MatchesController < ApplicationController
+
   def index
-matches = Match.all 
-render json: matches
+    matches = Match.all 
+    id = params[:player_id] 
+    player_matches = []
+      matches.each do |match| 
+        if match.id == id.to_i 
+          player_matches << match 
+          end
+        end
+    render json: player_matches
   end
+
+  def show 
+
+  end
+
   def create
   	
   end
+
   def destroy
   	
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
-    resources :matches
+    resources :players do 
+    	resources :matches 
+    end
   end
 end
