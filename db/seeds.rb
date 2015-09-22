ddc7a865b7aa388db3cff9e912166a8fc8f8096a
Victor Sagrista Lopez <victorsagristalopez@gmail.com>
2015-09-22 14:54:02 +0200

    Populated db with seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Player.destroy_all
+Match.destroy_all
+
+players = Player.create([
+{name: "player 1"},
+{name: "player 2"},
+{name: "player 3"}
+])
+
+matches = Match.create([
+	{winner_faction: "Terran", loser_faction: "Zerg", duration: 200000, date: Date.today, winner_id: players[0].id, loser_id: players[2].id},
+	{winner_faction: "Zerg", loser_faction: "Protoss", duration: 200000, date: Date.today, winner_id: players[1], loser_id: players[0].id},
+	{winner_faction: "Protoss", loser_faction: "Terran", duration: 200000, date: Date.today, winner_id: players[2], loser_id: players[1].id}
+])
