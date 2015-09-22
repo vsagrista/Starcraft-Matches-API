e808425d60e2ab7123e611612f76237a4deb9c52
Victor Sagrista Lopez <victorsagristalopez@gmail.com>
2015-09-22 17:07:28 +0200

    Second part of iteration 2, route to matches/faction that displays matches from that faction

--- app/controllers/api/matches_controller.rb
+++ app/controllers/api/matches_controller.rb
@@ -2,25 +2,30 @@ class Api::MatchesController < ApplicationController
 
   def index
     matches = Match.all 
-    id = params[:player_id] 
+    id = params[:player_id].to_i 
     player_matches = []
       matches.each do |match| 
-        if match.id == id.to_i 
+        if match.id == id
           player_matches << match 
           end
         end
     render json: player_matches
   end
 
+  def get_faction_matches 
+    matches = Match.all
+    faction = params[:faction].capitalize
+    matches_with_user_faction = []
+    matches.each do |match| 
+        if match.winner_faction == faction || match.loser_faction == faction
+          matches_with_user_faction << match 
+          end
+        end
+    render json: matches_with_user_faction
+  end
+
   def show 
 
-  end
-
-  def create
-  	
-  end
-
-  def destroy
   	
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  
   namespace :api do
+  	get ("/matches/:faction") => "matches#get_faction_matches"
+  	#resources :matches 
     resources :players do 
     	resources :matches 
     end
